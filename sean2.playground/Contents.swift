d8aa51b3a889d9cec88f80363aabeff714a8a130
Sean Levin <slevin@yahoo-inc.com>
2015-05-01 17:03:23 -0400

    don't know what I'm doing

---
+++ sean2.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import SpriteKit
+import XCPlayground
+
+let view:SKView = SKView(frame: CGRectMake(0, 0, 1024, 768))
+
+XCPShowView("Live View", view)
+
+let scene:SKScene = SKScene(size: CGSizeMake(1024, 768))
+scene.scaleMode = SKSceneScaleMode.AspectFit
+view.presentScene(scene);
+
+let redBox:SKSpriteNode = SKSpriteNode(color: SKColor.redColor(), size: CGSizeMake(300, 300))
+redBox.position = CGPointMake(512, 384)
+redBox.runAction(SKAction.repeatActionForever(SKAction.rotateByAngle(6, duration: 2)))
+scene.addChild(redBox)
